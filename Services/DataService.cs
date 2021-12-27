60780f7dce48407dd399cc555f95816b38d423f0
Gavin <gavin.g.walters@gmail.com>
2021-12-27 07:39:45 -0600

    cheeses

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -21,8 +21,9 @@ namespace IM.Services
 
         public List<string> Query() 
         {
+            string query = "SELECT * FROM inventory";
             List<string> list = new List<string>();
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM inventory", _connection);
+            NpgsqlCommand cmd = new NpgsqlCommand(query, _connection);
             using (var reader = cmd.ExecuteReader())
             {
                 while (reader.Read())
@@ -30,5 +31,6 @@ namespace IM.Services
             }
             return list;
         }
+        
     }
 }
